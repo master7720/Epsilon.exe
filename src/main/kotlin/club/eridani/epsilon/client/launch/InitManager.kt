4b1100f4153a6b461fa3a0a87d1d2e6738b514d6
Master7720 <80098736+master7720@users.noreply.github.com>
2024-01-30 12:58:14 -0800

    thx cryo

--- src/main/kotlin/club/eridani/epsilon/client/launch/InitManager.kt
+++ src/main/kotlin/club/eridani/epsilon/client/launch/InitManager.kt
@@ -1,7 +1,9 @@
 package club.eridani.epsilon.client.launch
 
 import club.eridani.epsilon.client.Epsilon
-
+import com.sun.org.apache.xpath.internal.operations.Mod
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 
 object InitManager {
 
@@ -15,13 +17,17 @@ object InitManager {
 
     }
 
+    @Suppress("UNUSED_PARAMETER")
+    @Mod.EventHandler
     @JvmStatic
-    fun preInitHook() {
+    fun preInitHook(event: FMLPreInitializationEvent) {
         Epsilon.preInit()
     }
 
+    @Suppress("UNUSED_PARAMETER")
+    @Mod.EventHandler
     @JvmStatic
-    fun postInitHook() {
+    fun postInitHook(event: FMLPostInitializationEvent) {
         Epsilon.postInit()
     }
 
